373b8cbc5e26facf4d46f57df0f8fb972cf4fbd7
Spectrum3900 <jeromeheuze@gmail.com>
2023-12-09 12:23:17 -0800

    up

--- web-app/nuxt.config.ts
+++ web-app/nuxt.config.ts
@@ -1,7 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: 'en'
+      },
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no',
+      meta: [
+        {name:"google-site-verification", content:"xxxxxxx"}
+      ],
+      link: [
+        {rel: 'shortcut icon',type:'image/x-icon', href: '/favicon.svg'}
+      ],
+      script: [ // valid options are: 'head' | 'bodyClose' | 'bodyOpen'
+
+      ]
+    }
+  },
   css: ['~/assets/scss/style.scss'],
-  modules: ['nuxt-gtag', '@nuxtjs/google-fonts', 'nuxt-icon-tw', '@nuxtjs/tailwindcss'],
+  modules: ['nuxt-gtag', '@nuxtjs/google-fonts', 'nuxt-icon-tw', '@nuxtjs/tailwindcss', 'nuxt-simple-sitemap', 'nuxt-simple-robots'],
   gtag: {
     id: 'G-XXXXXXXXXX'
   },
@@ -13,6 +31,16 @@ export default defineNuxtConfig({
       Raleway: '200..700',
       Inter: '200..700',
     }
+  },
+  sitemap: {
+    xslTips: false,
+    strictNuxtContentPaths: true
+  },
+  content: {
+    documentDriven: true
+  },
+  site: {
+    url: 'https://website-goes-here.com',
   },
   tailwindcss: {
     // Options
