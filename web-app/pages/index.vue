c9701491c531d473dee31e4b0c355eda6a90db48
Spectrum3900 <jeromeheuze@gmail.com>
2023-12-09 12:25:32 -0800

    Update index.vue

--- web-app/pages/index.vue
+++ web-app/pages/index.vue
@@ -1,3 +1,24 @@
 <template>
 
 </template>
+<script setup lang="ts">
+import {useHead,useSeoMeta} from "@unhead/vue";
+
+useHead({
+  link: [
+    {rel: 'canonical', href: 'https://website-goes-here.com'}
+  ]
+})
+
+useSeoMeta({
+  ogType: 'website',
+  title: 'A good Title.',
+  ogTitle: 'A good Title.',
+  description: 'A good description.',
+  ogDescription: 'A good description.',
+  ogImage: 'something.jpg',
+  twitterCard: 'summary_large_image',
+  ogUrl: 'https://website-goes-here.com'
+})
+
+</script>
